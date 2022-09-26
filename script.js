519b38494f58f89e93f647131141fd4084594aa6
lucacieduard <eduardlucaci2002@yahoo.com>
2022-09-26 14:55:08 +0300

    v1

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
 const hamBtn = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const links = Array.from(document.querySelectorAll(".links"));
-console.log(links)
+
 
 hamBtn.addEventListener("click",(e)=>{
     nav.classList.toggle("show-nav");
 })
 
-
+for(let i=0;i<links.length;i++){
+    links[i].addEventListener("click",()=>{
+        nav.classList.remove("show-nav");
+    })
+}
 
 
